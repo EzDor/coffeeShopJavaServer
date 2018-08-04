405368633cb6f404087be1fa5ac7b39b3d017a35
Asia <asia.shafran@gmail.com>
2018-08-05 01:22:31 +0300

    orderItem controller user ResponseEntity

--- src/main/java/com/legendary/coffeeShop/service/OrderItemService.java
+++ src/main/java/com/legendary/coffeeShop/service/OrderItemService.java
@@ -4,6 +4,8 @@ import com.legendary.coffeeShop.dao.entities.OrderItem;
 import com.legendary.coffeeShop.dao.repositories.OrderItemRepository;
 import com.legendary.coffeeShop.utils.Status;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +14,14 @@ public class OrderItemService {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
-    public Status removeOrderItem(int orderItemId) {
+    public ResponseEntity removeOrderItem(int orderItemId) {
         OrderItem orderItem = orderItemRepository.findById(orderItemId);
         if (orderItem == null) {
-            return new Status(Status.ERROR, String.format("Could not find order item with id %d", orderItemId));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(String.format("Could not find order item with id %d", orderItemId));
         }
         orderItemRepository.delete(orderItem);
-        return new Status(Status.OK, "Order updated successfully");
+        return ResponseEntity.status(HttpStatus.OK).body("Order updated successfully");
     }
 
 }
