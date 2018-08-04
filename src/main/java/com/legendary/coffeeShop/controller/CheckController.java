e29c478a972fbc3028793c3b6ddf360a88950590
Asia <asia.shafran@gmail.com>
2018-08-05 01:16:21 +0300

    check controller to responseEntity

--- src/main/java/com/legendary/coffeeShop/controller/CheckController.java
+++ src/main/java/com/legendary/coffeeShop/controller/CheckController.java
@@ -3,6 +3,8 @@ package com.legendary.coffeeShop.controller;
 import com.legendary.coffeeShop.utils.CommonConstants;
 import com.legendary.coffeeShop.utils.Status;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,18 +20,17 @@ public class CheckController {
 
     @GetMapping("/check")
     @ResponseBody
-    public Status check() {
+    public ResponseEntity check() {
         System.out.println("Check successful. constants:" + commonConstants.getTestCommonConstants());
-        Status status = new Status(Status.OK, "msg");
-        return status;
+        return ResponseEntity.status(HttpStatus.OK).body("msg");
+
     }
 
     @GetMapping("/checkAdmin")
     @PreAuthorize("hasRole('ADMIN')")
     @ResponseBody
-    public Status checkAdmin() {
+    public ResponseEntity checkAdmin() {
         System.out.println("Check successful. constants:" + commonConstants.getTestCommonConstants());
-        Status status = new Status(Status.OK, "msg");
-        return status;
+        return ResponseEntity.status(HttpStatus.OK).body("msg");
     }
 }
