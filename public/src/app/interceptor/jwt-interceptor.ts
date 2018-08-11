80d64b40ba814d575e46e73db6f40ed408ad8c37
Ez_Dor <Dor348@gmail.com>
2018-08-12 01:06:49 +0300

    Start build the home page menu with angular material cards and grid
    (cherry picked from commit 6dc4d4c)

--- public/src/app/interceptor/jwt-interceptor.ts
+++ public/src/app/interceptor/jwt-interceptor.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Constants} from '../model/constants';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // add authorization header with jwt token if available
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const currentUser = JSON.parse(localStorage.getItem(Constants.CURRENT_USER));
     if (currentUser && currentUser.token) {
       request = request.clone({
         setHeaders: {
